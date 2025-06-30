30b5485cc2ebc9fb04dfc61126b5d0c1ea3efb4c
Robert McArthur <robert.mcarthur@anu.edu.au>
2025-06-30 15:37:41 +1000

    DEV: remove another debug echo

--- build_tools/build_iqtree.sh
+++ build_tools/build_iqtree.sh
@@ -12,8 +12,6 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 elif [[ "$OSTYPE" == "msys"* || "$OSTYPE" == "cygwin"* ]]; then
     echo "Building for Windows."
 
-    echo "My PATH is: $PATH"
-
     if [[ -n "$BOOST_ROOT" ]]; then
         export Boost_INCLUDE_DIR="${BOOST_ROOT}"
         export Boost_LIBRARY_DIRS="${BOOST_ROOT}"
